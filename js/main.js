ac70844db51eec71ef8e2cfd1e8b4db93db7b3c5
João Araújo <146267273+zenuncro@users.noreply.github.com>
2024-06-20 20:57:20 +0100

    resize game screen

--- js/main.js
+++ js/main.js
@@ -14,6 +14,10 @@ const config = {
         preload: preload,
         create: create,
         update: update
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
     }
 };
 
